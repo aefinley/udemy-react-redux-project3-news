35e978b95fec23a28ef8af515f5592fe8f9ad0fb
allison finley <allisonfinley42@gmail.com>
2019-09-06 17:10:59 -0500

    Setting up reducers

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { homedir } from 'os';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+}
+
+export default Home;
